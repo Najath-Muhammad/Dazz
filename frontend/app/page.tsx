38b224f4488a784ab4e829e5bdb9a8e89b83e34d
Najath <najathmuhammad22@gmail.com>
2026-08-30 14:50:55 +0530

    Wrap RootHomePage in PublicLayout so Navbar and Footer render on root route

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,3 +1,10 @@
+import PublicLayout from './(public)/layout';
 import HomePage from './(public)/page';
 
-export default HomePage;
+export default function RootHomePage() {
+  return (
+    <PublicLayout>
+      <HomePage />
+    </PublicLayout>
+  );
+}
